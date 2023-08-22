fcc10a0dd82e4eb6e2519abcb91ac336a5674959
semenkucesvilin <142807133+semenkucesvilin@users.noreply.github.com>
2023-08-22 22:52:08 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,10 +3,13 @@ package ideaProjects;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("My name is Semen.");
-        System.out.println(5 + 10);
-        System.out.println(" -> ");
+        System.out.println("    +");
+        System.out.println("   +++");
+        System.out.println("  +++++");
+        System.out.println(" +++++++");
+        System.out.println("+++++++++");
+        System.out.println("   +++");
+        System.out.println("   +++");
 
     }
 }
